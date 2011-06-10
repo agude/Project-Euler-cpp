109d226497b40f33904fa658f862bd2b8b8eecb7
Alexander Gude <alex.public.account+ProjectEulerSolutions@gmail.com>
2011-06-10 15:52:14 +0200

    Updating Problem 96 in C++ to use a new structure to pass coordinates around.

--- Problem096.cpp
+++ Problem096.cpp
@@ -35,38 +35,38 @@
 #include    <stdlib.h>
 #include    <string>
 
+struct Coordinate{
+    short col;
+    short row;
+};
+
+/*
+ * =====================================================================================
+ *        Class:  Grid
+ *  Description:  Stores a Sudoku board and solves it
+ * =====================================================================================
+ */
 class Grid {
     private:
         // Gameboard
         short m_grid[9][9];
         bool  m_possible[9][9][9];
-        short* returnRow( short i, short j);
-        short* returnCol( short i, short j);
-        short* returnSqr( short i, short j);
+        Coordinate* returnRow( short i, short j);
+        Coordinate* returnCol( short i, short j);
+        Coordinate* returnSqr( short i, short j);
         void pullNeighbors( short i, short j);
-        void pushNeightbrs( short i, short j, short val);
+        void pullConstraints( short i, short j);
         void checkCell( short i, short j);
         short strtoint(char str);
 
     public:
         Grid(std::string inPuzzle);
         void printGrid();
-
-        // Creates a nice way to access the gameboard
-        struct Cells{
-            short* A1,* A2,* A3,* A4,* A5,* A6,* A7,* A8,* A9;
-            short* B1,* B2,* B3,* B4,* B5,* B6,* B7,* B8,* B9;
-            short* C1,* C2,* C3,* C4,* C5,* C6,* C7,* C8,* C9;
-            short* D1,* D2,* D3,* D4,* D5,* D6,* D7,* D8,* D9;
-            short* E1,* E2,* E3,* E4,* E5,* E6,* E7,* E8,* E9;
-            short* F1,* F2,* F3,* F4,* F5,* F6,* F7,* F8,* F9;
-            short* G1,* G2,* G3,* G4,* G5,* G6,* G7,* G8,* G9;
-            short* H1,* H2,* H3,* H4,* H5,* H6,* H7,* H8,* H9;
-            short* I1,* I2,* I3,* I4,* I5,* I6,* I7,* I8,* I9;
-        } m_cells;
-
 };
 
+/*-----------------------------------------------------------------------------
+ *  Construtor
+ *-----------------------------------------------------------------------------*/
 Grid::Grid (std::string inPuzzle){
     // Blank gameboard and possibilites
     for ( short i=0 ; i<9 ; i++ ) {
@@ -83,70 +83,54 @@ Grid::Grid (std::string inPuzzle){
             }
         }
     }
-/* 
-    for ( short i=0 ; i<9 ; i++ ) {
-        for ( short j=0 ; j<9 ; j++ ) {
-            std::cout << m_grid[i][j] << ": ";
-            for ( short k=0 ; k<9 ; k++ ) {
-                std::cout << k << ':' << m_possible[i][j][k] << ", ";
-            }
-            std::cout << std::endl;
-        }
-    }
- */
-    // Assigns all the poshorters need for m_cells
-    m_cells.A1 = &m_grid[0][0]; m_cells.A2 = &m_grid[0][1]; m_cells.A3 = &m_grid[0][2]; m_cells.A4 = &m_grid[0][3]; m_cells.A5 = &m_grid[0][4]; m_cells.A6 = &m_grid[0][5]; m_cells.A7 = &m_grid[0][6]; m_cells.A8 = &m_grid[0][7]; m_cells.A9 = &m_grid[0][8];
-    m_cells.B1 = &m_grid[1][0]; m_cells.B2 = &m_grid[1][1]; m_cells.B3 = &m_grid[1][2]; m_cells.B4 = &m_grid[1][3]; m_cells.B5 = &m_grid[1][4]; m_cells.B6 = &m_grid[1][5]; m_cells.B7 = &m_grid[1][6]; m_cells.B8 = &m_grid[1][7]; m_cells.B9 = &m_grid[1][8];
-    m_cells.C1 = &m_grid[2][0]; m_cells.C2 = &m_grid[2][1]; m_cells.C3 = &m_grid[2][2]; m_cells.C4 = &m_grid[2][3]; m_cells.C5 = &m_grid[2][4]; m_cells.C6 = &m_grid[2][5]; m_cells.C7 = &m_grid[2][6]; m_cells.C8 = &m_grid[2][7]; m_cells.C9 = &m_grid[2][8];
-    m_cells.D1 = &m_grid[3][0]; m_cells.D2 = &m_grid[3][1]; m_cells.D3 = &m_grid[3][2]; m_cells.D4 = &m_grid[3][3]; m_cells.D5 = &m_grid[3][4]; m_cells.D6 = &m_grid[3][5]; m_cells.D7 = &m_grid[3][6]; m_cells.D8 = &m_grid[3][7]; m_cells.D9 = &m_grid[3][8];
-    m_cells.E1 = &m_grid[4][0]; m_cells.E2 = &m_grid[4][1]; m_cells.E3 = &m_grid[4][2]; m_cells.E4 = &m_grid[4][3]; m_cells.E5 = &m_grid[4][4]; m_cells.E6 = &m_grid[4][5]; m_cells.E7 = &m_grid[4][6]; m_cells.E8 = &m_grid[4][7]; m_cells.E9 = &m_grid[4][8];
-    m_cells.F1 = &m_grid[5][0]; m_cells.F2 = &m_grid[5][1]; m_cells.F3 = &m_grid[5][2]; m_cells.F4 = &m_grid[5][3]; m_cells.F5 = &m_grid[5][4]; m_cells.F6 = &m_grid[5][5]; m_cells.F7 = &m_grid[5][6]; m_cells.F8 = &m_grid[5][7]; m_cells.F9 = &m_grid[5][8];
-    m_cells.G1 = &m_grid[6][0]; m_cells.G2 = &m_grid[6][1]; m_cells.G3 = &m_grid[6][2]; m_cells.G4 = &m_grid[6][3]; m_cells.G5 = &m_grid[6][4]; m_cells.G6 = &m_grid[6][5]; m_cells.G7 = &m_grid[6][6]; m_cells.G8 = &m_grid[6][7]; m_cells.G9 = &m_grid[6][8];
-    m_cells.H1 = &m_grid[7][0]; m_cells.H2 = &m_grid[7][1]; m_cells.H3 = &m_grid[7][2]; m_cells.H4 = &m_grid[7][3]; m_cells.H5 = &m_grid[7][4]; m_cells.H6 = &m_grid[7][5]; m_cells.H7 = &m_grid[7][6]; m_cells.H8 = &m_grid[7][7]; m_cells.H9 = &m_grid[7][8];
-    m_cells.I1 = &m_grid[8][0]; m_cells.I2 = &m_grid[8][1]; m_cells.I3 = &m_grid[8][2]; m_cells.I4 = &m_grid[8][3]; m_cells.I5 = &m_grid[8][4]; m_cells.I6 = &m_grid[8][5]; m_cells.I7 = &m_grid[8][6]; m_cells.I8 = &m_grid[8][7]; m_cells.I9 = &m_grid[8][8];
 
     printGrid();
-
-    for ( int z=0; z<1000; z++){
+    for ( int z=0; z<100; z++){
         for ( short i=0 ; i<9 ; i++ ) {
             for ( short j=0 ; j<9 ; j++ ) {
+                checkCell(i,j);
                 pullNeighbors(i,j);
+                checkCell(i,j);
+                pullConstraints(i,j);
                 checkCell(i,j);
             }
         }
     }
-
     std::cout << std::endl;
-
     printGrid();
 }
 
-short* Grid::returnRow( short i, short j) {
-    short* row = new short[8];
+/*-----------------------------------------------------------------------------
+ *  Return the coordinates of groupings of cells (rows, cols, squares)
+ *-----------------------------------------------------------------------------*/
+Coordinate* Grid::returnRow( short i, short j) {
+    Coordinate* Row = new Coordinate[8];
     short l=0;
     for ( short k=0; k<9; k++) {
         if ( k != j ) { // We don't want the cell we're looking at in the row
-            row[l] = m_grid[i][k];
+            Row[l].row = i;
+            Row[l].col = k;
             l++;
         }
     }
-    return row;
+    return Row;
 }
 
-short* Grid::returnCol( short i, short j) {
-    short* col = new short[8];
+Coordinate* Grid::returnCol( short i, short j) {
+    Coordinate* Col = new Coordinate[8];
     short l=0;
     for ( short k=0; k<9; k++) {
         if ( k != i ) { // We don't want the cell we're looking at in the col
-            col[l] = m_grid[k][j];
+            Col[l].row = k;
+            Col[l].col = j;
             l++;
         }
     }
-    return col;
+    return Col;
 }
 
-short* Grid::returnSqr( short i, short j) {
-    short* sqr = new short[8];
+Coordinate* Grid::returnSqr( short i, short j) {
+    Coordinate* Sqr = new Coordinate[8];
     short rowStart, rowEnd;
     short colStart, colEnd;
 
@@ -206,67 +190,134 @@ short* Grid::returnSqr( short i, short j) {
     for ( short k=rowStart; k < rowEnd; k++ ) {
         for ( short l=colStart; l < colEnd; l++ ) {
             if (i != k || j != l) {
-                sqr[m] = m_grid[k][l];
-                //std::cout << m_grid[k][l] << " grid[" << k << "][" << l << ']' << std::endl;
+                Sqr[m].row = k;
+                Sqr[m].col = l;
                 m++;
             }
         }
     }
-    return sqr;
+    return Sqr;
 }
 
+/*-----------------------------------------------------------------------------
+ *  Our two propegation methods. 
+ *-----------------------------------------------------------------------------*/
 void Grid::pullNeighbors( short i, short j) {
     /* 
-      Given a cell at i,j, c, checks all other cells connected to c
+      Given a cell, c at i,j, checks all other cells connected to c
       and removes values from c's possiblity array.
      */       
+    //std::cout << "Checking " << i << ' ' << j << ':' << std::endl;
+
     bool* pos[9]; 
     for ( short k=0; k<9; k++) {
         pos[k] = &m_possible[i][j][k];
+        //std::cout << ' ' << k+1 << ':' << *pos[k];
     }
+    //std::cout << std::endl;
 
-    //std::cout << "Checking m_grid[" << i << "][" << j << "] = " << m_grid[i][j] << std::endl;
-
-    short* sqr;
+    Coordinate* sqr;
     sqr = returnSqr(i,j);
     for ( short k = 0; k<8; k++){ 
-        if (sqr[k] != 0) {
-            //std::cout << "\t" << "Found neighbor with value: " << sqr[k] << std::endl;
-            *pos[sqr[k]-1] = false;
+        short m = sqr[k].row;
+        short n = sqr[k].col;
+        short val = m_grid[m][n];
+        if (val != 0) {
+            //std::cout << "\tFound value " << val << " at " << m << ' ' << n << std::endl;
+            *pos[val-1] = false;
         }
     }
 
-    short* row;
+    Coordinate* row;
     row = returnRow(i,j);
-    for ( short k = 0; k<8; k++){
-        if (row[k] != 0) {
-            //std::cout << "\t" << "Found neighbor with value: " << row[k] << std::endl;
-            *pos[row[k]-1] = false;
+    for ( short k = 0; k<8; k++){ 
+        short m = row[k].row;
+        short n = row[k].col;
+        short val = m_grid[m][n];
+        if (val != 0) {
+            //std::cout << "\tFound value " << val << " at " << m << ' ' << n << std::endl;
+            *pos[val-1] = false;
         }
     }
 
-    short* col;
+    Coordinate* col;
     col = returnCol(i,j);
-    for ( short k = 0; k<8; k++){
-        if (col[k] != 0) {
-            //std::cout << "\t" << "Found neighbor with value: " << col[k] << std::endl;
-            *pos[col[k]-1] = false;
+    for ( short k = 0; k<8; k++){ 
+        short m = col[k].row;
+        short n = col[k].col;
+        short val = m_grid[m][n];
+        if (val != 0) {
+            //std::cout << "\tFound value " << val << " at " << m << ' ' << n << std::endl;
+            *pos[val-1] = false;
         }
     }
 
     for ( short k=0; k<9; k++) {
-        //std::cout << *pos[k];
+        pos[k] = &m_possible[i][j][k];
+        //std::cout << ' ' << k+1 << ':' << *pos[k];
     }
     //std::cout << std::endl;
-    for ( short k=0; k<9; k++) {
-        //std::cout << m_possible[i][j][k];
-    }
     //std::cout << std::endl;
-
 }
 
-void Grid::pushNeightbrs( short i, short j, short val) {
+void Grid::pullConstraints( short i, short j){
+    /* 
+     * If a cell is the only sell in its row/col/sqr
+     * that can take a value, it must be that value
+     */
+    bool onlyHere[8]; 
+    for ( short l=0; l<9; l++) {
+        onlyHere[l] = m_possible[i][j][l];
+    }
 
+    Coordinate* sqr;
+    sqr = returnSqr(i,j);
+    for ( short k = 0; k<8; k++){ 
+        short  m = sqr[k].row;
+        short  n = sqr[k].col;
+        for ( short l=0; l<9; l++) {
+            if (m_possible[m][n][l]) {
+                onlyHere[l]=false;
+            }
+        }
+    }
+
+    Coordinate* row;
+    row = returnRow(i,j);
+    for ( short k = 0; k<8; k++){ 
+        short  m = row[k].row;
+        short  n = row[k].col;
+        for ( short l=0; l<9; l++) {
+            if (m_possible[m][n][l]) {
+                onlyHere[l]=false;
+            }
+        }
+    }
+
+    Coordinate* col;
+    col = returnCol(i,j);
+    for ( short k = 0; k<8; k++){ 
+        short  m = col[k].row;
+        short  n = col[k].col;
+        for ( short l=0; l<9; l++) {
+            if (m_possible[m][n][l]) {
+                onlyHere[l]=false;
+            }
+        }
+    }
+
+    short ntrue=0;
+    short val=0;
+    for ( short l=0; l<9; l++) {
+        if (onlyHere[l]) {
+            val = l+1;
+            ntrue++;
+        }
+    }
+
+    if (ntrue == 1) {
+       m_grid[i][j] = val; 
+    }
 }
 
 void Grid::checkCell( short i, short j) {
@@ -286,6 +337,9 @@ void Grid::checkCell( short i, short j) {
     }
 }
 
+/*-----------------------------------------------------------------------------
+ *  Method to print the grid nicely
+ *-----------------------------------------------------------------------------*/
 void Grid::printGrid() {
     using namespace std;
 
@@ -304,6 +358,9 @@ void Grid::printGrid() {
 }
 
 
+/*-----------------------------------------------------------------------------
+ *  Method to convert str to int. Better ways exist, needs replacement.
+ *-----------------------------------------------------------------------------*/
 short Grid::strtoint(char str){
     if (str == '0') { 
         return 0;
@@ -330,6 +387,13 @@ short Grid::strtoint(char str){
     }
 }
 
+
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  main
+ *  Description:  Runs our solver
+ * =====================================================================================
+ */
 int main () { 
     //std::string s1="003020600900305001001806400008102900700000008006708200002609500800203009005010300";
     std::string s1="080903040006107000003000600600089004900000003700640002009000300000806200010705090";
